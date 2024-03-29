1d159ca7bdd2f073ac95372a209a73665e614d7d
VISH01AL <g.vish1al@gmail.com>
2024-03-28 23:00:00 -0400

    added final project start

--- MyCode/dns_fingerprinting_main.py
+++ MyCode/dns_fingerprinting_main.py
@@ -7,6 +7,8 @@ from scapy.all import *
 from collections import defaultdict
 from datetime import datetime
 
+rrnameset=set()
+rdataset=set()
 
 def get_subdirectories(directory):
     subdirectories = []
@@ -25,44 +27,46 @@ def get_pcap_files(directory):
     return pcap_files
 
 
+
+
+
+
 def extract_mdns_data(pcap_file):
     qtype_mapping = {
-    1: "A", 2: "NS", 3: "MD", 4: "MF", 5: "CNAME",
-    6: "SOA", 7: "MB", 8: "MG", 9: "MR", 10: "NULL",
-    11: "WKS", 12: "PTR", 13: "HINFO", 14: "MINFO",
-    15: "MX", 16: "TXT", 17: "RP", 18: "AFSDB", 19: "X25",
-    20: "ISDN", 21: "RT", 22: "NSAP", 23: "NSAP_PTR",
-    24: "SIG", 25: "KEY", 26: "PX", 27: "GPOS", 28: "AAAA",
-    29: "LOC", 30: "NXT", 31: "EID", 32: "NIMLOC", 33: "SRV",
-    34: "ATMA", 35: "NAPTR", 36: "KX", 37: "CERT", 38: "A6",
-    39: "DNAME", 40: "SINK", 41: "OPT", 42: "APL", 43: "DS",
-    44: "SSHFP", 45: "IPSECKEY", 46: "RRSIG", 47: "NSEC",
-    48: "DNSKEY", 49: "DHCID", 50: "NSEC3", 51: "NSEC3PARAM",
-    52: "TLSA", 53: "SMIMEA", 54: "HIP", 55: "NINFO", 56: "RKEY",
-    57: "TALINK", 58: "CDS", 59: "CDNSKEY", 60: "OPENPGPKEY",
-    61: "CSYNC", 62: "ZONEMD", 63: "SVCB", 64: "HTTPS", 65: "SPF",
-    99: "UINFO", 100: "UID", 101: "GID", 102: "UNSPEC", 103: "NID",
-    104: "L32", 105: "L64", 106: "LP", 107: "EUI48", 108: "EUI64",
-    109: "TKEY", 249: "TKEY", 250: "TSIG", 251: "IXFR", 252: "AXFR",
-    253: "MAILB", 254: "MAILA", 255: "ANY",
-    **{i: "RESERVED" for i in range(32770, 65536)}  # Handling reserved qtypes
+        1: "A", 2: "NS", 3: "MD", 4: "MF", 5: "CNAME",
+        6: "SOA", 7: "MB", 8: "MG", 9: "MR", 10: "NULL",
+        11: "WKS", 12: "PTR", 13: "HINFO", 14: "MINFO",
+        15: "MX", 16: "TXT", 17: "RP", 18: "AFSDB", 19: "X25",
+        20: "ISDN", 21: "RT", 22: "NSAP", 23: "NSAP_PTR",
+        24: "SIG", 25: "KEY", 26: "PX", 27: "GPOS", 28: "AAAA",
+        29: "LOC", 30: "NXT", 31: "EID", 32: "NIMLOC", 33: "SRV",
+        34: "ATMA", 35: "NAPTR", 36: "KX", 37: "CERT", 38: "A6",
+        39: "DNAME", 40: "SINK", 41: "OPT", 42: "APL", 43: "DS",
+        44: "SSHFP", 45: "IPSECKEY", 46: "RRSIG", 47: "NSEC",
+        48: "DNSKEY", 49: "DHCID", 50: "NSEC3", 51: "NSEC3PARAM",
+        52: "TLSA", 53: "SMIMEA", 54: "HIP", 55: "NINFO", 56: "RKEY",
+        57: "TALINK", 58: "CDS", 59: "CDNSKEY", 60: "OPENPGPKEY",
+        61: "CSYNC", 62: "ZONEMD", 63: "SVCB", 64: "HTTPS", 65: "SPF",
+        99: "UINFO", 100: "UID", 101: "GID", 102: "UNSPEC", 103: "NID",
+        104: "L32", 105: "L64", 106: "LP", 107: "EUI48", 108: "EUI64",
+        109: "TKEY", 249: "TKEY", 250: "TSIG", 251: "IXFR", 252: "AXFR",
+        253: "MAILB", 254: "MAILA", 255: "ANY",
+        **{i: "RESERVED" for i in range(32770, 65536)}  # Handling reserved qtypes
     }
-
 
     qclass_mapping = {
-    1: "IN",  # Internet
-    2: "CS",  # CSNET (Obsolete)
-    3: "CH",  # CHAOS
-    4: "HS",  # Hesiod
-    **{i: "RESERVED" for i in range(32768, 65536)}  # Handling reserved qclasses
+        1: "IN",  # Internet
+        2: "CS",  # CSNET (Obsolete)
+        3: "CH",  # CHAOS
+        4: "HS",  # Hesiod
+        **{i: "RESERVED" for i in range(32768, 65536)}  # Handling reserved qclasses
     }
 
-
     opcode_mapping = {
-    0: "Query",
-    1: "Inverse Query",
-    2: "Status",
-    **{i: "RESERVED" for i in range(3, 16)}  # Handling reserved opcodes
+        0: "Query",
+        1: "Inverse Query",
+        2: "Status",
+        **{i: "RESERVED" for i in range(3, 16)}  # Handling reserved opcodes
     }
 
     mdns_data = defaultdict(list)
@@ -76,6 +80,7 @@ def extract_mdns_data(pcap_file):
                 if pkt.haslayer(DNSQR):
                     domain = pkt[DNS].qd.qname.decode('utf-8')
                     dest_ip = pkt[IP].dst
+                    src_ip = pkt[IP].src
                     timestamp = float(pkt.time)
                     date = datetime.fromtimestamp(timestamp)
                     id = pkt[DNS].id
@@ -86,95 +91,66 @@ def extract_mdns_data(pcap_file):
                     aa = pkt[DNS].aa
                     opcode = pkt[DNS].opcode
 
-
                     # Map numeric values to human-readable strings
                     qtype_str = qtype_mapping.get(qtype, "Unknown")
                     qclass_str = qclass_mapping.get(qclass, "Unknown")
                     opcode_str = opcode_mapping.get(opcode, "Unknown")
 
+                    # Check if it's a response packet and extract response type
+                    if not pkt[DNS].qr:
+                        response_type = qtype_str
+                    else:
+                        response_type = "Response"
+
+                    # Extract resource records
+                    rrname = []
+                    rdata = []
+                    if pkt.haslayer(DNSRR):
+                        for rr in pkt[DNSRR]:
+                            rrname.append(rr.rrname.decode('utf-8'))
+                            rdata.append(rr.rdata)
+
                     # Store the mDNS data
-                    mdns_data[domain].append({
-                        "destination_ip": dest_ip,
-                        "date": date,
-                        "id": id,
-                        "qtype": qtype,
-                        "qclass": qclass,
-                        "qtype_human_readable": qtype_str,
-                        "qclass_human_readable": qclass_str,
-                        "rd": rd,
-                        "tc": tc,
-                        "aa": aa,
-                        "opcode": opcode,
-                        "opcode_human_readable": opcode_str
-                    })
+                    for x in rrname:
+                        rrnameset.add(x)
+                    for x in rdata:
+                        if type(x)==str:
+                            rdataset.add(x)
+                        elif type(x)==bytes:
+                            rdataset.add(str(x))
+                        else:
+                            print("==================",type(x))
+                    if len(rrname)!=0 or len(rdata)!=0:
+                        mdns_data[domain].append({
+                            "source_ip": src_ip,
+                            "destination_ip": dest_ip,
+                            "date": date,
+                            "id": id,
+                            "qtype": qtype,
+                            "qclass": qclass,
+                            "qtype_human_readable": qtype_str,
+                            "qclass_human_readable": qclass_str,
+                            "rd": rd,
+                            "tc": tc,
+                            "aa": aa,
+                            "opcode": opcode,
+                            "opcode_human_readable": opcode_str,
+                            "response_type": response_type,
+                            "rrname": rrname,
+                            "rdata": rdata
+                        })
+    print()
+    print()
+    print(rdataset)
+    print(rrnameset)
+    print()
+    print()
 
     return mdns_data
 
 
 
-# Function to extract DNS data from pcap files
-# Function to extract DNS data from pcap files
-def extract_dns_data(pcap_file):
-    # Define a dictionary to store DNS data
-    dns_data = defaultdict(list)
 
-    # Mapping dictionaries for qtype, qclass, and opcode
-    qtype_mapping = {
-        1: "A",
-        2: "NS",
-        5: "CNAME",
-        15: "MX",
-        28: "AAAA",
-        # Add more mappings as needed
-    }
-
-    qclass_mapping = {
-        1: "IN",
-        # Add more mappings as needed
-    }
-
-    opcode_mapping = {
-        0: "Query",
-        1: "Inverse Query",
-        2: "Status",
-        # Add more mappings as needed
-    }
-
-    pkts = rdpcap(pcap_file)
-    for pkt in pkts:
-        if pkt.haslayer(DNS) and pkt.haslayer(IP):
-            if pkt[DNS].qr == 0:
-                # Extract DNS data from DNS query packets
-                domain = pkt[DNS].qd.qname.decode('utf-8')
-                dest_ip = pkt[IP].dst
-                timestamp = float(pkt.time)
-                date = datetime.fromtimestamp(timestamp)
-                id = pkt[DNS].id
-                qtype = pkt[DNS].qd.qtype
-                qtype_str = qtype_mapping.get(qtype, "Unknown")  # Get the string representation from the mapping
-                qclass = pkt[DNS].qd.qclass
-                qclass_str = qclass_mapping.get(qclass, "Unknown")  # Get the string representation from the mapping
-                rd = pkt[DNS].rd
-                tc = pkt[DNS].tc
-                aa = pkt[DNS].aa
-                opcode = pkt[DNS].opcode
-                opcode_str = opcode_mapping.get(opcode, "Unknown")  # Get the string representation from the mapping
-                dns_data[domain].append({
-                    "destination_ip": dest_ip,
-                    "date": date,
-                    "id": id,
-                    "qtype": qtype,
-                    "qclass": qclass,
-                    "qtype_human_readable": qtype_str,
-                    "qclass_human_readable": qclass_str,
-                    "rd": rd,
-                    "tc": tc,
-                    "aa": aa,
-                    "opcode": opcode,
-                    "opcode_human_readable": opcode_str  # New variable to store human-readable opcode
-                })
-
-    return dns_data
 
 def print_Dns(dns_data):
     for domain, entries in dns_data.items():
@@ -207,9 +183,10 @@ def write_dns_data_to_json(dns_data,directory,subdirectory,filename):
 def main():
     
 
-    #directory from where all the subdirectories should contain pcap files 
+   
     
-    directory = "./AUSTRALIA_PCAPS"
+    # directory = "./Australia" #directory from where all the subdirectories should contain pcap files 
+    directory = "./India"
     subdirectories = get_subdirectories(directory)
    
 
